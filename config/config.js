58cc73803e3299bd1c8b4fd208f974a9826a1f1b
hwupathum <46132469+hwupathum@users.noreply.github.com>
2019-12-21 11:45:39 +0530

    hosted db online

--- config/config.js
+++ config/config.js
@@ -1,15 +1,15 @@
-var config = {
-  database: {
-      host: 'localhost',     // database host
-      user: 'root',         // your database username
-      password: '',         // your database password
-      port: 3306,         // default MySQL port
-      db: 'hr_system'         // your database name
-  },
-  server: {
-      host: '127.0.0.1',
-      port: '3000'
-  }
-}
+const config = {
+    database: {
+        host: 'sql12.freemysqlhosting.net',     // database host
+        user: 'sql12316521',         // your database username
+        password: 'hrcCrHDVLb',         // your database password
+        port: 3306,         // default MySQL port
+        db: 'sql12316521'         // your database name
+    },
+    server: {
+        host: '127.0.0.1',
+        port: '3000'
+    }
+};
 
-module.exports = config
+module.exports = config;
