23e8b21f2134c77f4383d5f78cffb8d66f9c0f3d
hwupathum <46132469+hwupathum@users.noreply.github.com>
2020-01-02 02:04:27 +0530

    group report

--- routes/index.js
+++ routes/index.js
@@ -62,7 +62,7 @@ router.post('/employee/:id/apply', user_landing.check_authenticated, user_landin
 
 // REPORTS
 router.get('/reports/department/:department_id', user_landing.check_authenticated, user_landing.show_report_dept);
-router.get('/reports/job/:job_id', user_landing.check_authenticated, user_landing.show_report_job);
+router.get('/reports/group', user_landing.check_authenticated, user_landing.show_report_group);
 // router.get('/custom_attr/add', admin_landing.check_authenticated, admin_landing.show_add_custom_attr);
 
 module.exports = router;
